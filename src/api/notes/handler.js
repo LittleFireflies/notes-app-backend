a61e3596260bc4e56ce6619c18d4e77b7f924a7b
Widyarso Joko Purnomo <widyjp10@gmail.com>
2022-11-10 18:41:38 +0700

    implement notes validator

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -1,7 +1,8 @@
 /* eslint-disable require-jsdoc */
 class NotesHandler {
-  constructor(service) {
+  constructor(service, validator) {
     this._service = service;
+    this._validator = validator;
 
     this.postNoteHandler = this.postNoteHandler.bind(this);
     this.getNotesHandler = this.getNotesHandler.bind(this);
@@ -12,6 +13,8 @@ class NotesHandler {
 
   postNoteHandler(request, h) {
     try {
+      this._validator.validateNotePayload(request.payload);
+
       const {title = 'Untitled', body, tags} = request.payload;
 
       const noteId = this._service.addNote({title, body, tags});
@@ -69,6 +72,8 @@ class NotesHandler {
 
   putNoteByIdHandler(request, h) {
     try {
+      this._validator.validateNotePayload(request.payload);
+
       const {id} = request.params;
 
       this._service.editNoteById(id, request.payload);
